a10cca18786ce37736f77c1be2669a613b567c53
Marcus Myrick <mjmyrick12@gmail.com>
2022-11-06 19:59:09 -0600

    added Dates

--- main.js
+++ main.js
@@ -22,4 +22,21 @@ function getResults (query) {
 
 function displayResults (weather) {
     console.log(weather);
+    let city = document.querySelector('.location .city');
+    city.innerText = `${weather.name}, ${weather.sys.country}`;
+
+    let now = new Date();
+    let dat = document.querySelector('.location .date');
+    Date.innerText = dateBuilder(now);
+}
+
+function dateBuilder (d) {
+    let months = ["January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December"];
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday",
+    "Friday", "Saturday"];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    
 }
